fd3d3f24ff8ff4baaf89d92386ad23cd17f7b0a1
Loner123 <dlwlstjq410@gmail.com>
2021-02-09 03:11:25 +0900

    [Modify] BaseFragment추가

--- app/src/main/java/kr/loner/fx/ui/dest/main/ShareFragment.kt
+++ app/src/main/java/kr/loner/fx/ui/dest/main/ShareFragment.kt
@@ -1,26 +1,14 @@
 package kr.loner.fx.ui.dest.main
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
+import kr.loner.fx.R
 import kr.loner.fx.databinding.FragmentShareBinding
+import kr.loner.fx.ui.base.BaseFragment
+import kr.loner.fx.viewmodel.MainViewModel
 
-class ShareFragment : Fragment() {
-
-    lateinit var binding:FragmentShareBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = FragmentShareBinding.inflate(layoutInflater,container,false).run{
-        binding = this
-        root
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+class ShareFragment : BaseFragment<FragmentShareBinding>(R.layout.fragment_share,
+MainViewModel::class.java) {
+    override fun FragmentShareBinding.setDataBind() {
 
     }
 
